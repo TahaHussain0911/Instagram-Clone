76b723a66915e94da16892aa76e5fdcb2b0bdb53
Taha <tahahussain0805@gmail.com>
2023-02-25 10:52:35 +0500

    Images Added

--- src/context/InstaState.js
+++ src/context/InstaState.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import InstaContext from "./InstaContext";
 import amas from '../Profileimages/amas.jpg'
 import faiq from '../Profileimages/faiq.jpg'
+import faiqpost from '../Profileimages/faiqpost.jpg'
 import fouz from '../Profileimages/fouz.jpg'
 import noor from '../Profileimages/noor.jpg'
 import rabi from '../Profileimages/rabi.jpg'
@@ -16,6 +17,16 @@ import talha from '../Profileimages/talha.jpg'
 import azam from '../Profileimages/azam.jpg'
 import farooq from '../Profileimages/farooq.jpg'
 import fakhir from '../Profileimages/fakhir.jpg'
+import sameer from '../Profileimages/sameer.jpg'
+import sameerpost from '../Profileimages/sameerpost.jpg'
+import shayan from '../Profileimages/shayan.jpg'
+import aqib from '../Profileimages/aqib.jpg'
+import zain from '../Profileimages/zain.jpg'
+import zainpost from '../Profileimages/zainpost.jpg'
+
+
+
+
 
 
 const InstaState = (props) => {
@@ -268,33 +279,33 @@ const InstaState = (props) => {
       id:12,
       name:"faiq.umer",
       profile:faiq,
-      image: "https://scontent.fkhi16-1.fna.fbcdn.net/v/t1.6435-9/130957955_2498143543818901_469961598866206187_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_eui2=AeE1R_gnHfnh4P7wc7YC5AaluS26k8LExYC5LbqTwsTFgAyqjmsfHSvnR7423oippFHbgb8JZaoI_4HL09rVOvwk&_nc_ohc=gVFfMKrYMsQAX_LXOrc&_nc_oc=AQkfapSbgRePcBl59kle3dhsjhrDZOAQKBpffxiO3nB1ZQlic3UgnwCIVzpGj3sf4ag&_nc_ht=scontent.fkhi16-1.fna&oh=00_AfDcwTW1e3PNjx-TKlZQnEdfJAaLBi4nRPRzCwGoxR_BIg&oe=64192E18",
+      image: faiqpost,
       posted: "1d",
       comments: "10",
       caption: "Hold my hand and I'll prove not everyone leaves 🖤",
       liked: "aqib_mehtab 92 others",
-      likedimage: "https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/272890297_3122150054670477_3100786536773693556_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeENbto_shg27QguSmLOhXeSa7I1C-e8Zj1rsjUL57xmPcvGZDrnHqK3Wyu1pMeQokkwb-LBBmL_h3YC1S3PxCyO&_nc_ohc=bt43-6mN99EAX9qaDBg&_nc_ht=scontent.fkhi16-1.fna&oh=00_AfARG4DZoRNuir9ZpKJWEgcXbqi96yEL6f_Ym0ybvxLRlw&oe=63F6552C",
+      likedimage: aqib,
       border:true,
       message :[]
     },
     {
       id: 212,
       name:"_sameersidd._",
-      profile:"https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/321908337_480188084192516_1031501553856039307_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGWSYyXimKQZIoiRevt8YcRaKqs-I3G45hoqqz4jcbjmAqRmvo2QSCa9Z3K2EFtxuNwQHzDjdqcUy775EgQ_96j&_nc_ohc=1W5zdLUEF9UAX_O6AIe&_nc_ht=scontent.fkhi16-1.fna&oh=00_AfAJDGTrLdhO58rInHy81Klrsk0j3K2qL_fVLVCmJ0iFTg&oe=63F5C763",
-      image: "https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/260675798_2680081178960643_6492317174193418292_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=174925&_nc_eui2=AeE-Qb7-nKiAhWcFUOx1LGZ2J7MA8gKD1RQnswDyAoPVFDBcWvvQuTSaxSe3-Lvrrk0hX0ZOF46hDVlegvQX16Xr&_nc_ohc=Ihh4Zs9OQokAX9L9pX7&_nc_ht=scontent.fkhi16-1.fna&oh=00_AfB3XIv76cS0WV8pkN9cWtxPXhxW9wkUuCyWzlAQB5QKBQ&oe=63F6CB58",
+      profile:sameer,
+      image: sameerpost,
       posted: "12h",
       comments: "6",
       caption: "Coping ok.",
       liked: "shyn_mhmood 59 others",
-      likedimage: "https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/314599918_1785758038458912_4982266317743827956_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF1Yc8b8zmZAcV4-xgInCsBIICzJk34_1wggLMmTfj_XFo8Za4UjRpI5Y4ZpI1Mj1Bvj87Va3CrjTqsbgh5nvgF&_nc_ohc=sqfY8O4xikUAX9Qu7gI&_nc_ht=scontent.fkhi16-1.fna&oh=00_AfCGGQXQfLx3PxxkMjdSMvkfjOb4uFO3cdog3HmUaQAZ7Q&oe=63F68944",
+      likedimage: shayan,
       border:false,
       message :[]
     },
     {
       id:213,
       name:"xain.rehman",
-      profile:"https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/279713471_7619212374763082_4824274654927912082_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGxvJ-ks73zUgyZxWZh1S9e60nGzemIRkXrScbN6YhGRTsoROIEaRIoA_M3wuA_D8LZCzxtU7oS5s57MqdjmQfW&_nc_ohc=cLaWBs6dQTcAX8L4I2O&_nc_ht=scontent.fkhi16-1.fna&oh=00_AfDiLdzdzNGvq6RDdb7ok6APLZ-5N3zZW4MG_tfIUC7USQ&oe=63F7E938",
-      image: "https://scontent.fkhi16-1.fna.fbcdn.net/v/t39.30808-6/231216263_6121556204528714_5658534278383112711_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHkCLY7Rjg6xJ0753TL0dGf4CqmBJ0CA3PgKqYEnQIDc9Nfy60xPsxBH3NNHWm19CUI1Vp28uVEjbO0SleRONhH&_nc_ohc=2eEiObzAP0cAX95GNPQ&tn=8yrXWYui61hPE9Ah&_nc_ht=scontent.fkhi16-1.fna&oh=00_AfC0z6tz4g8ysR5IiWYJeJm1ux3D8yGPfi7MS8eomxqxXw&oe=63F8A3A5",
+      profile: zain,
+      image: zainpost,
       posted: "3d",
       comments: "15",
       caption: "🥀✨تیرے بن نی لگدا دل میرا ڈھولنا",
